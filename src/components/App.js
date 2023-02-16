20b3b8be040c0a580f029cb9df5a2d54bcda67c2
ynitin2906 <ynitin2906@gmail.com>
2023-02-16 23:34:56 +0530

    update

--- src/components/App.js
+++ src/components/App.js
@@ -4,20 +4,20 @@ const arr = JSON.parse(
   window.localStorage.getItem("props") || `["hello","world"]`
 ); // do not change
 
-const users = ["hello", "newton", "school"];
+const words = ["hello", "newton", "school"];
 
 const Join = (props) => {
   return (
     <div id="join">
       {/* Access prop 'words' and print it using .join like words.join(',')*/}
-      {users.join(",")}
+      {words.join(",")}
     </div>
   );
 };
 const App = () => {
   return (
     <div id="main">
-      <Join {...users} />
+      <Join {...words} />
     </div>
   );
 };
